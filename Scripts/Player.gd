2b9beed1e0c235ed592f1accd21a415b22a34730
SpicyBacon <sandyde123@gmail.com>
2021-07-20 20:50:36 +0530

    Added Base Player Movement

---
+++ Scripts/Player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+const SPEED = 200
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+
+func get_input():
+	velocity = Vector2()
+	
+	var up = Input.is_action_pressed("Up")
+	var down = Input.is_action_pressed("Down")
+	var left = Input.is_action_pressed("Left")
+	var right = Input.is_action_pressed("Right")
+	
+	if up:
+		velocity.y -= 1
+	if down:
+		velocity.y += 1
+	if left:
+		velocity.x -= 1
+	if right:
+		velocity.x += 1
+	velocity = velocity.normalized() * SPEED
