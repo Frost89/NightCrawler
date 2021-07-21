0d15be391d62083a6013bdbbeb752c2f1a7d20ff
MangoDesktop <moinakdhar25@gmail.com>
2021-07-21 20:34:19 +0530

    Movement code update

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,26 +1,23 @@
 extends KinematicBody2D
 
-var velocity = Vector2()
-const SPEED = 200
+const maxspeed = 150
+const acceleration = 600
+const friction = 1200
+
+var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	get_input()
-	velocity = move_and_slide(velocity)
+	
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	input_vector.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * maxspeed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	move_and_slide(velocity)
 
-func get_input():
-	velocity = Vector2()
-	
-	var up = Input.is_action_pressed("Up")
-	var down = Input.is_action_pressed("Down")
-	var left = Input.is_action_pressed("Left")
-	var right = Input.is_action_pressed("Right")
-	
-	if up:
-		velocity.y -= 1
-	if down:
-		velocity.y += 1
-	if left:
-		velocity.x -= 1
-	if right:
-		velocity.x += 1
-	velocity = velocity.normalized() * SPEED
