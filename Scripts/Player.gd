ce46e1a8a0a20ea0c79c17c69a8b86e839d510ce
SpicyBacon <sandyde123@gmail.com>
2021-07-23 16:51:58 +0530

    Spicy #1 Fixed front walk glide. Added right walk. Changed locomation anim to animTree.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,31 +6,27 @@ const friction = 1200
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $MainChar/AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
 func _physics_process(delta):
-	
+	move(delta)
+	move_and_slide(velocity)
+
+func move(delta):
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	input_vector.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 	input_vector = input_vector.normalized()
 	
-	if input_vector != Vector2.ZERO:
-		
-		if input_vector.x >0:                            #animation code
-			$MainChar.play("walk left")                  
-			$MainChar.flip_h =true
-		else:
-			$MainChar.play("walk left")                  #animation code
-			$MainChar.flip_h = false
-			
+	if input_vector != Vector2.ZERO:               
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationState.travel("Walk")
 		velocity = velocity.move_toward(input_vector * maxspeed, acceleration * delta)
-		
-		if input_vector.y !=0:
-			$MainChar.play("front walk")                 
-	
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-		$MainChar.play("idle")
 	
-	move_and_slide(velocity)
-
